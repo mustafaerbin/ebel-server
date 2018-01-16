f5d442d984fa4a8fbb504647eb5ef14087066dfd
Mustafa Erbin <mustafa.erbin@mebitech.com>
2018-01-16 09:17:59 +0300

    ctv

--- ebel-server-web/src/main/java/com/tr/danismend/ebel/web/controller/CtvTarifesiController.java
+++ ebel-server-web/src/main/java/com/tr/danismend/ebel/web/controller/CtvTarifesiController.java
@@ -1,9 +1,15 @@
 package com.tr.danismend.ebel.web.controller;
 
+import com.tr.danismend.ebel.domain.CtvTarifesi;
 import com.tr.danismend.ebel.services.CtvTarifesiServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Created by Mustafa Erbin on 1/15/2018
@@ -14,4 +20,11 @@ public class CtvTarifesiController {
 
     @Autowired
     CtvTarifesiServices ctvTarifesiServices;
+
+    @GetMapping(value = "{yil}")
+    public List<CtvTarifesi> listeCtvTarifesiYil(@PathVariable("yil") int yil) {
+        return ctvTarifesiServices.listeCtvTarifesiYil(yil);
+    }
+
+
 }
